cdca9ecc396805a2050362af1e2734ae3c32c8c6
KrisssBrown <cristianbruno.cs@gmail.com>
2023-02-03 16:33:44 +0100

    START

---
+++ JS/app.js
@@ -0,0 +1,7 @@
+// console.log('ciao')
+
+const listaMail = ['pincopallo90@gmail.com', 'tizio@gmail.com', 'caio68@gmail.com', 'league.of.lecce@ gmail.com', 'biondotwix.82@gmail.com '];
+
+console.dir (listaMail, listaMail.lenght)
+
+// creare array
