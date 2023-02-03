52946077c86318621607f6037ca2952aa0aa606e
KrisssBrown <cristianbruno.cs@gmail.com>
2023-02-03 17:48:32 +0100

    END mail checker

--- JS/app.js
+++ JS/app.js
@@ -8,16 +8,25 @@ const listaMail = ['pincopallo90@gmail.com', 'tizio@gmail.com', 'caio68@gmail.co
 console.log (listaMail, listaMail.lenght)
 
 let nuovaMail = prompt('Inserisci la tua mail')
+let conferma = false
 
 for ( i = 0; i < listaMail.length; i++){
 
     if (nuovaMail === listaMail[i]) {
-        console.log (i,'email corretta')
-        alert('Mail trovata!')
+        //  console.log (i,'email corretta')
+        //  alert('Mail trovata!')
+         conferma = true
         
     }
     else{
-        console.log(i, 'mail inesistente')
+        //  console.log(i, 'mail inesistente')
     }
-    
 }
+
+if (conferma === true){
+alert ('Mail trovata!')
+}
+else{
+     alert ('Mail non trovata!')
+}
+
