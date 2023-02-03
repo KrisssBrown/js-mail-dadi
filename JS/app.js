98e6bda0feff190b913dcc0968878af2094b1c5c
KrisssBrown <cristianbruno.cs@gmail.com>
2023-02-03 17:01:40 +0100

    FEAT mail checker add

--- JS/app.js
+++ JS/app.js
@@ -1,7 +1,23 @@
 // console.log('ciao')
 
-const listaMail = ['pincopallo90@gmail.com', 'tizio@gmail.com', 'caio68@gmail.com', 'league.of.lecce@ gmail.com', 'biondotwix.82@gmail.com '];
 
-console.dir (listaMail, listaMail.lenght)
 
 // creare array
+const listaMail = ['pincopallo90@gmail.com', 'tizio@gmail.com', 'caio68@gmail.com', 'league.of.lecce@gmail.com', 'biondotwix.82@gmail.com']
+
+console.log (listaMail, listaMail.lenght)
+
+let nuovaMail = prompt('Inserisci la tua mail')
+
+for ( i = 0; i < listaMail.length; i++){
+
+    if (nuovaMail === listaMail[i]) {
+        console.log (i,'email corretta')
+        alert('Mail trovata!')
+        
+    }
+    else{
+        console.log(i, 'mail inesistente')
+    }
+    
+}
